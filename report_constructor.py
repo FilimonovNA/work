8d4b695b61fbe759830fdf3ef7f8d1fcbbcf090d
FilimonovNA <filimonovn.a@ya.ru>
2022-09-02 18:08:22 +0300

    develop add table and code optimisation

--- report_constructor.py
+++ report_constructor.py
@@ -1,8 +1,10 @@
 import os
 from tkinter import filedialog
 from docx import Document
-from docx.shared import Pt, Cm, RGBColor
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt, Cm, RGBColor, Inches
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_TAB_ALIGNMENT
+
+'''Добавить функции вычисления номера этажа и номера скрипта картинки'''
 
 
 def select_path():
@@ -22,10 +24,10 @@ def get_pictures_list(user_path):
 
 
 def add_picture_in_file(picture, doc):
-    doc.add_picture(path_with_pictures + '/' + picture, width=Pt(500))
+    doc.add_picture(path_with_pictures + '/' + picture, width=Pt(480))
 
 
-def margin_set(doc):
+def set_margin(doc):
     sections = doc.sections
     for section in sections:
         section.top_margin = Cm(2)
@@ -34,14 +36,19 @@ def margin_set(doc):
         section.right_margin = Cm(1)
 
 
+def get_picture_number_of_floor(picture):
+    if picture[1].isdigit():
+        floor_num = picture[:2]
+    else:
+        floor_num = picture[0]
+    return floor_num
+
+
 def get_floor_list(pictures_list):
     elem = ''
     floor_list = []
     for elem in pictures_list:
-        if elem[1].isdigit():
-            floor_num = elem[:2]
-        else:
-            floor_num = elem[0]
+        floor_num = get_picture_number_of_floor(elem)
         if floor_num not in floor_list:
             floor_list.append(floor_num)
     return sorted(floor_list)
@@ -55,31 +62,47 @@ def add_floor_title_in_file(floor, doc):
     doc.add_paragraph()
 
 
-def floor_pictures(floor, pictures_list):
-    floor_pictures_list = []
-    for elem in pictures_list:
-        if elem[1].isdigit():
-            pic_num = elem[:2]
-        else:
-            pic_num = elem[0]
-        if str(floor) == pic_num:
-            floor_pictures_list.append(elem)
-    return floor_pictures_list
+def floor_pictures(current_floor, _all_pictures):
+    pictures_list = []
+    for elem in _all_pictures:
+        pic_num = get_picture_number_of_floor(elem)
+        if str(current_floor) == pic_num:
+            pictures_list.append(elem)
+    return pictures_list
+
+
+def table_adjusting(tab):
+    for cell in tab.columns[0].cells:
+        cell.width = Cm(7.5)
+    for col in tab.columns:
+        for cell in col.cells:
+            cell.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
 
 def add_table_with_values(doc):
-    table = doc.add_table(rows=4, cols=4)
+    rows = 4
+    cols = 4
+    table = doc.add_table(rows=rows, cols=cols)
     table.rows[0].cells[1].text = '2G'
     table.rows[0].cells[2].text = '3G'
     table.rows[0].cells[3].text = '4G'
+
     table.rows[1].cells[0].text = 'Средний уровень сигнала, дБм'
     table.rows[2].cells[0].text = 'Cредняя скорость DL, Мб/c'
     table.rows[3].cells[0].text = 'Cредняя скорость UL, Мб/c'
-    table.rows[2].cells[1].text = '-'  # DL 2G
-    table.rows[3].cells[1].text = '-'  # UL 2G
+    table.rows[1].cells[1].text = 'Meas Rx Level'           # Rx level
+    table.rows[2].cells[1].text = '-'                       # DL 2G
+    table.rows[3].cells[1].text = '-'                       # UL 2G
+    table.rows[1].cells[2].text = 'RSCP'                    # RSCP
+    table.rows[2].cells[2].text = 'DL3G'                    # DL 3G
+    table.rows[3].cells[2].text = 'UL3G'                    # UL 3G
+    table.rows[1].cells[3].text = 'RSRP'                    # RSRP
+    table.rows[2].cells[3].text = 'DL4G'                    # DL 2G
+    table.rows[3].cells[3].text = 'UL4G'                    # UL 2G
+    table_adjusting(table)
 
 
-def app_picture_caption(picture, doc, serial_number):
+def add_picture_caption(picture, doc, serial_number):
     picture_captions = {
     '01': 'Качественные показатели покрытия 2G', '02': 'Качественные показатели покрытия 3G',
     '03': 'Качественные показатели покрытия 4G', '04': 'Скорость ППД DL/UL 3G',
@@ -92,12 +115,20 @@ def app_picture_caption(picture, doc, serial_number):
     picture_caption = picture_captions.get(picture_number_on_floor)
     paragraph = doc.add_paragraph('')
     text = paragraph.add_run(f'Рисунок {serial_number} - {picture_caption}')
-    paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
     text.font.name = 'Times new roman'
     text.font.size = Pt(14)
 
 
+'''Если номер картинки == 9, 10, 11 - выводим add scanner title'''
 
+
+def add_scanner_title(doc):
+    floor_title = doc.add_heading().add_run('Функциональные показатели со сканера')
+    floor_title.font.name = 'Times new roman'
+    floor_title.font.size = Pt(14)
+    floor_title.font.color.rgb = RGBColor(0, 0, 0)
+    doc.add_paragraph()
 
 
 def construct_floor(report_doc, floor, floor_pictures_list, absolut_picture_number_in_file):
@@ -105,7 +136,7 @@ def construct_floor(report_doc, floor, floor_pictures_list, absolut_picture_numb
     add_table_with_values(report_doc)
     for picture in floor_pictures_list:
         add_picture_in_file(picture, report_doc)
-        app_picture_caption(picture, report_doc, absolut_picture_number_in_file)
+        add_picture_caption(picture, report_doc, absolut_picture_number_in_file)
         absolut_picture_number_in_file += 1
     report_doc.add_page_break()
     return absolut_picture_number_in_file
@@ -113,7 +144,7 @@ def construct_floor(report_doc, floor, floor_pictures_list, absolut_picture_numb
 
 path = 'C:/Users/PC/Desktop/Work/'  # legacy for save time
 report_doc = Document()
-margin_set(report_doc)
+set_margin(report_doc)
 path_with_pictures = path + '/Pictures'
 # path_with_pictures = select_path()
 all_pictures = get_pictures_list(path_with_pictures)

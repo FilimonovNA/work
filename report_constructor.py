c4d56e08a41a9d8b34fd6eda39422d8af1e6de0e
FilimonovNA <filimonovn.a@ya.ru>
2022-09-02 20:38:56 +0300

    develop refactor some funtions

--- report_constructor.py
+++ report_constructor.py
@@ -14,6 +14,13 @@ def select_path():
     return user_path
 
 
+def save_report(_doc, _path):
+    try:
+        _doc.save(_path + '/test.docx')
+    except PermissionError:
+        return 1
+
+
 def get_pictures_list(user_path):
     all_files = os.listdir(user_path)
     list_of_pictures = []
@@ -24,8 +31,10 @@ def get_pictures_list(user_path):
 
 
 def add_picture_in_file(picture, doc):
-    doc.add_picture(path_with_pictures + '/' + picture, width=Pt(480))
-
+    paragraph = doc.add_paragraph()
+    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    run = paragraph.add_run("")
+    run.add_picture(path_with_pictures + '/' + picture, width=Pt(480))
 
 def set_margin(doc):
     sections = doc.sections
@@ -54,8 +63,8 @@ def get_floor_list(pictures_list):
     return sorted(floor_list)
 
 
-def add_floor_title_in_file(floor, doc):
-    floor_title = doc.add_heading().add_run(f'{floor} этаж')
+def add_floor_title_in_file(current_floor, doc):
+    floor_title = doc.add_heading().add_run(f'{current_floor} этаж')
     floor_title.font.name = 'Times new roman'
     floor_title.font.size = Pt(16)
     floor_title.font.color.rgb = RGBColor(0, 0, 0)
@@ -71,14 +80,6 @@ def floor_pictures(current_floor, _all_pictures):
     return pictures_list
 
 
-def table_adjusting(tab):
-    for cell in tab.columns[0].cells:
-        cell.width = Cm(7.5)
-    for col in tab.columns:
-        for cell in col.cells:
-            cell.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-
-
 def add_table_with_values(doc):
     rows = 4
     cols = 4
@@ -86,20 +87,35 @@ def add_table_with_values(doc):
     table.rows[0].cells[1].text = '2G'
     table.rows[0].cells[2].text = '3G'
     table.rows[0].cells[3].text = '4G'
-
     table.rows[1].cells[0].text = 'Средний уровень сигнала, дБм'
     table.rows[2].cells[0].text = 'Cредняя скорость DL, Мб/c'
     table.rows[3].cells[0].text = 'Cредняя скорость UL, Мб/c'
-    table.rows[1].cells[1].text = 'Meas Rx Level'           # Rx level
+    table.rows[1].cells[1].text = 'Rx Level'           # Rx level
     table.rows[2].cells[1].text = '-'                       # DL 2G
     table.rows[3].cells[1].text = '-'                       # UL 2G
     table.rows[1].cells[2].text = 'RSCP'                    # RSCP
-    table.rows[2].cells[2].text = 'DL3G'                    # DL 3G
-    table.rows[3].cells[2].text = 'UL3G'                    # UL 3G
+    table.rows[2].cells[2].text = 'DL 3G'                    # DL 3G
+    table.rows[3].cells[2].text = 'UL 3G'                    # UL 3G
     table.rows[1].cells[3].text = 'RSRP'                    # RSRP
-    table.rows[2].cells[3].text = 'DL4G'                    # DL 2G
-    table.rows[3].cells[3].text = 'UL4G'                    # UL 2G
-    table_adjusting(table)
+    table.rows[2].cells[3].text = 'DL 4G'                    # DL 2G
+    table.rows[3].cells[3].text = 'UL 4G'                    # UL 2G
+    doc.add_paragraph()
+    table_adjusting(table, rows, cols)
+    # table = doc.add_table(1, 2)
+    # table.cell(0, 0).text = 'Left alignment'
+    # table.cell(0, 0).paragraphs[0].paragraph_format.alignment = WD_TAB_ALIGNMENT.LEFT
+    # table.cell(0, 1).text = 'Right alignment'
+    # table.cell(0, 1).paragraphs[0].paragraph_format.alignment = WD_TAB_ALIGNMENT.RIGHT
+    # table_adjusting(table)
+
+
+def table_adjusting(tab, rows, cols):
+    tab.alignment = WD_TAB_ALIGNMENT.CENTER
+    for cell in tab.columns[0].cells:
+        cell.width = Cm(7.5)
+    for i in range(rows):
+        for j in range(1, cols):
+            tab.cell(i, j).paragraphs[0].paragraph_format.alignment = WD_TAB_ALIGNMENT.CENTER
 
 
 def add_picture_caption(picture, doc, serial_number):
@@ -108,8 +124,8 @@ def add_picture_caption(picture, doc, serial_number):
     '03': 'Качественные показатели покрытия 4G', '04': 'Скорость ППД DL/UL 3G',
     '05': 'Скорость ППД DL/UL 4G', '06': 'Функциональные показатели CSFB',
     '07': 'Функциональные показатели LTE Carrier Aggregation',
-    '08': 'Функциональные показатели LTE MIMO', '09': 'Функциональные показатели 2G indoor',
-    '10': 'Функциональные показатели 3G indoor', '11': 'Функциональные показатели LTE indoor'
+    '08': 'Функциональные показатели LTE MIMO', '09': 'Функциональные показатели 2G indoor scanner',
+    '10': 'Функциональные показатели 3G indoor scanner', '11': 'Функциональные показатели LTE indoor scanner'
     }
     picture_number_on_floor = picture[picture.find("fl")+3:picture.find("fl")+5]
     picture_caption = picture_captions.get(picture_number_on_floor)
@@ -149,17 +165,16 @@ path_with_pictures = path + '/Pictures'
 # path_with_pictures = select_path()
 all_pictures = get_pictures_list(path_with_pictures)
 all_floors = get_floor_list(all_pictures)
-
-
-absolut_picture_number_in_file = 1
-for floor in all_floors:
-    floor_pictures_list = floor_pictures(floor, all_pictures)  # получаем список картинок для конкретного этажа
-    absolut_picture_number_in_file = construct_floor(report_doc, floor, floor_pictures_list, absolut_picture_number_in_file)  # создаем конструкцию первого этажа
 # report_path = select_path()
 report_path = path
 
-try:
-    report_doc.save(report_path + '/test.docx')
-    print('SUCCESS')
-except PermissionError:
-    print('Close the file pls')
+if save_report(report_doc, report_path) != 1:
+    absolut_picture_number_in_file = 1
+    for floor in all_floors:
+        floor_pictures_list = floor_pictures(floor, all_pictures)  # получаем список картинок для конкретного этажа
+        absolut_picture_number_in_file = construct_floor(report_doc, floor, floor_pictures_list,
+                                                         absolut_picture_number_in_file)  # создаем этаж
+    save_report(report_doc, report_path)
+    print("SUCCESS")
+else:
+    print("CLOSE FILE")

f541690224e87d0a95f7418b1ed4d008828f233b
FilimonovNA <filimonovn.a@ya.ru>
2022-09-03 11:48:14 +0300

    develop add scanner_title, picture_floor, picture_script and some refactor

--- report_constructor.py
+++ report_constructor.py
@@ -1,19 +1,20 @@
 import os
 from tkinter import filedialog
 from docx import Document
-from docx.shared import Pt, Cm, RGBColor, Inches
+from docx.shared import Pt, Cm, RGBColor
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_TAB_ALIGNMENT
 
-'''Добавить функции вычисления номера этажа и номера скрипта картинки'''
 
-
+# Открытие диалогового окна и выбор юзером папки
 def select_path():
     user_path = ''
     while user_path == '':
         user_path = filedialog.askdirectory(title="Select a File")
+        # Возвращает строку содержащую путь к выбранной папке
     return user_path
 
 
+# Принимает на вход название документа и путь сохраняет документ если это возможно, иначе возвращает 1
 def save_report(_doc, _path):
     try:
         _doc.save(_path + '/test.docx')
@@ -21,6 +22,7 @@ def save_report(_doc, _path):
         return 1
 
 
+# На основании полученного на вход пути возвращает список строк содержащих названия картинок в папке
 def get_pictures_list(user_path):
     all_files = os.listdir(user_path)
     list_of_pictures = []
@@ -30,12 +32,15 @@ def get_pictures_list(user_path):
     return list_of_pictures
 
 
+# Добавляет 1 картинку файл
 def add_picture_in_file(picture, doc):
     paragraph = doc.add_paragraph()
     paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
     run = paragraph.add_run("")
     run.add_picture(path_with_pictures + '/' + picture, width=Pt(480))
 
+
+# Настройка полей для документа, можно добавить доп настройки: размер документа, ориентация и т.д.
 def set_margin(doc):
     sections = doc.sections
     for section in sections:
@@ -45,6 +50,7 @@ def set_margin(doc):
         section.right_margin = Cm(1)
 
 
+# Возвращает номер этажа, на котором будет располагаться данная картинка
 def get_picture_number_of_floor(picture):
     if picture[1].isdigit():
         floor_num = picture[:2]
@@ -53,8 +59,14 @@ def get_picture_number_of_floor(picture):
     return floor_num
 
 
+# Возвращает порядковый номер скрипта картинки для функции add_picture_caption
+def get_script_number_of_picture(picture):
+    script_num = picture[picture.find("fl")+3:picture.find("fl")+5]
+    return script_num
+
+
+# Возвращает отсортированный список этажей
 def get_floor_list(pictures_list):
-    elem = ''
     floor_list = []
     for elem in pictures_list:
         floor_num = get_picture_number_of_floor(elem)
@@ -63,6 +75,7 @@ def get_floor_list(pictures_list):
     return sorted(floor_list)
 
 
+# Добавляет заголовок этажа и форматирует его
 def add_floor_title_in_file(current_floor, doc):
     floor_title = doc.add_heading().add_run(f'{current_floor} этаж')
     floor_title.font.name = 'Times new roman'
@@ -71,7 +84,8 @@ def add_floor_title_in_file(current_floor, doc):
     doc.add_paragraph()
 
 
-def floor_pictures(current_floor, _all_pictures):
+# Возвращает список картинок для конкретного этажа
+def get_list_of_floor_pictures(current_floor, _all_pictures):
     pictures_list = []
     for elem in _all_pictures:
         pic_num = get_picture_number_of_floor(elem)
@@ -80,6 +94,8 @@ def floor_pictures(current_floor, _all_pictures):
     return pictures_list
 
 
+# Добавляет таблицу со значениями для каждого этажа
+# Необходима доработка для вычисляемых значений из немо(чтение из файла)
 def add_table_with_values(doc):
     rows = 4
     cols = 4
@@ -90,26 +106,21 @@ def add_table_with_values(doc):
     table.rows[1].cells[0].text = 'Средний уровень сигнала, дБм'
     table.rows[2].cells[0].text = 'Cредняя скорость DL, Мб/c'
     table.rows[3].cells[0].text = 'Cредняя скорость UL, Мб/c'
-    table.rows[1].cells[1].text = 'Rx Level'           # Rx level
+    table.rows[1].cells[1].text = 'Rx Level'                # Rx level
     table.rows[2].cells[1].text = '-'                       # DL 2G
     table.rows[3].cells[1].text = '-'                       # UL 2G
     table.rows[1].cells[2].text = 'RSCP'                    # RSCP
-    table.rows[2].cells[2].text = 'DL 3G'                    # DL 3G
-    table.rows[3].cells[2].text = 'UL 3G'                    # UL 3G
+    table.rows[2].cells[2].text = 'DL 3G'                   # DL 3G
+    table.rows[3].cells[2].text = 'UL 3G'                   # UL 3G
     table.rows[1].cells[3].text = 'RSRP'                    # RSRP
-    table.rows[2].cells[3].text = 'DL 4G'                    # DL 2G
-    table.rows[3].cells[3].text = 'UL 4G'                    # UL 2G
+    table.rows[2].cells[3].text = 'DL 4G'                   # DL 2G
+    table.rows[3].cells[3].text = 'UL 4G'                   # UL 2G
     doc.add_paragraph()
-    table_adjusting(table, rows, cols)
-    # table = doc.add_table(1, 2)
-    # table.cell(0, 0).text = 'Left alignment'
-    # table.cell(0, 0).paragraphs[0].paragraph_format.alignment = WD_TAB_ALIGNMENT.LEFT
-    # table.cell(0, 1).text = 'Right alignment'
-    # table.cell(0, 1).paragraphs[0].paragraph_format.alignment = WD_TAB_ALIGNMENT.RIGHT
-    # table_adjusting(table)
+    adjust_table_with_values(table, rows, cols)
 
 
-def table_adjusting(tab, rows, cols):
+# Форматирование таблицы со значениями
+def adjust_table_with_values(tab, rows, cols):
     tab.alignment = WD_TAB_ALIGNMENT.CENTER
     for cell in tab.columns[0].cells:
         cell.width = Cm(7.5)
@@ -118,28 +129,30 @@ def table_adjusting(tab, rows, cols):
             tab.cell(i, j).paragraphs[0].paragraph_format.alignment = WD_TAB_ALIGNMENT.CENTER
 
 
+# Добавление названия к картинке
 def add_picture_caption(picture, doc, serial_number):
-    picture_captions = {
-    '01': 'Качественные показатели покрытия 2G', '02': 'Качественные показатели покрытия 3G',
-    '03': 'Качественные показатели покрытия 4G', '04': 'Скорость ППД DL/UL 3G',
-    '05': 'Скорость ППД DL/UL 4G', '06': 'Функциональные показатели CSFB',
-    '07': 'Функциональные показатели LTE Carrier Aggregation',
-    '08': 'Функциональные показатели LTE MIMO', '09': 'Функциональные показатели 2G indoor scanner',
-    '10': 'Функциональные показатели 3G indoor scanner', '11': 'Функциональные показатели LTE indoor scanner'
-    }
-    picture_number_on_floor = picture[picture.find("fl")+3:picture.find("fl")+5]
-    picture_caption = picture_captions.get(picture_number_on_floor)
+    picture_captions = {'01': 'Качественные показатели покрытия 2G', '02': 'Качественные показатели покрытия 3G',
+                        '03': 'Качественные показатели покрытия 4G', '04': 'Скорость ППД DL/UL 3G',
+                        '05': 'Скорость ППД DL/UL 4G', '06': 'Функциональные показатели CSFB',
+                        '07': 'Функциональные показатели LTE Carrier Aggregation',
+                        '08': 'Функциональные показатели LTE MIMO', '09': 'Функциональные показатели 2G indoor scanner',
+                        '10': 'Функциональные показатели 3G indoor scanner',
+                        '11': 'Функциональные показатели LTE indoor scanner'
+                        }
+    script_number_of_picture = get_script_number_of_picture(picture)
+    picture_caption = picture_captions.get(script_number_of_picture)
     paragraph = doc.add_paragraph('')
     text = paragraph.add_run(f'Рисунок {serial_number} - {picture_caption}')
+
+    # Форматирование подписи
     paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
     text.font.name = 'Times new roman'
     text.font.size = Pt(14)
 
 
-'''Если номер картинки == 9, 10, 11 - выводим add scanner title'''
-
-
+# Добавление подписи для раздела измерений со сканера
 def add_scanner_title(doc):
+    doc.add_page_break()
     floor_title = doc.add_heading().add_run('Функциональные показатели со сканера')
     floor_title.font.name = 'Times new roman'
     floor_title.font.size = Pt(14)
@@ -147,17 +160,24 @@ def add_scanner_title(doc):
     doc.add_paragraph()
 
 
-def construct_floor(report_doc, floor, floor_pictures_list, absolut_picture_number_in_file):
-    add_floor_title_in_file(floor, report_doc)
-    add_table_with_values(report_doc)
-    for picture in floor_pictures_list:
-        add_picture_in_file(picture, report_doc)
-        add_picture_caption(picture, report_doc, absolut_picture_number_in_file)
-        absolut_picture_number_in_file += 1
-    report_doc.add_page_break()
-    return absolut_picture_number_in_file
+# Полная "Сборка" 1 этажа для отчета
+def add_floor_in_report(doc, current_floor, current_floor_pictures_list, picture_number_in_file):
+    add_floor_title_in_file(current_floor, doc)
+    add_table_with_values(doc)
+    is_scanner_title_was_add = 0
+    for picture in current_floor_pictures_list:
+        script_number = get_script_number_of_picture(picture)
+        if script_number in ['09', '10', '11'] and is_scanner_title_was_add == 0:
+            add_scanner_title(doc)
+            is_scanner_title_was_add = 1
+        add_picture_in_file(picture, doc)
+        add_picture_caption(picture, doc, picture_number_in_file)
+        picture_number_in_file += 1
+    doc.add_page_break()
+    return picture_number_in_file
 
 
+'''Аля main, в котором происходит сборка всего отчета и вызов функций'''
 path = 'C:/Users/PC/Desktop/Work/'  # legacy for save time
 report_doc = Document()
 set_margin(report_doc)
@@ -171,9 +191,9 @@ report_path = path
 if save_report(report_doc, report_path) != 1:
     absolut_picture_number_in_file = 1
     for floor in all_floors:
-        floor_pictures_list = floor_pictures(floor, all_pictures)  # получаем список картинок для конкретного этажа
-        absolut_picture_number_in_file = construct_floor(report_doc, floor, floor_pictures_list,
-                                                         absolut_picture_number_in_file)  # создаем этаж
+        floor_pictures_list = get_list_of_floor_pictures(floor, all_pictures)
+        absolut_picture_number_in_file = add_floor_in_report(report_doc, floor, floor_pictures_list,
+                                                             absolut_picture_number_in_file)
     save_report(report_doc, report_path)
     print("SUCCESS")
 else:

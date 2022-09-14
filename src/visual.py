ef188edf496d541a8644b52862a0552ab4745ce4
FilimonovNA <filimonovn.a@ya.ru>
2022-09-14 20:27:48 +0300

    develop add select path

--- src/visual.py
+++ src/visual.py
@@ -5,19 +5,29 @@ window = Tk()
 window.title("Report Generator")
 window.geometry('700x400')
 
+
 def clicked():
     user_path = filedialog.askdirectory(title="Select a File")
-    label_path.configure(text = user_path)
-    print(label_path['text'])
+    #label_path.configure(show=user_path)
+    label_path.config(text=user_path)
+    window.title(user_path)
 
+
+def config():
+    print(window.title)
+
+
+user_path = ""
 label_select_path = Label(window, text = "Select path with pictures")
 label_select_path.grid(column=1, row=0)
 
-btn_select_path = Button(window, text="Select", command=clicked)
-btn_select_path.grid(column=1, row=2)
+btn_select_path = Button(window, text="Click", command=clicked)
+btn_select_path.grid(column=1, row=1)
 
-label_path = Label(window, text="Yours path will be here")
-label_path.grid(column=1, row=4)
+label_path = Label(window, text = "path")
+label_path.grid(column=1, row=2)
 
+btn_configure = Button(window, text="Configure", command=config)
+btn_configure.grid(column=2, row=1)
 
 window.mainloop()
